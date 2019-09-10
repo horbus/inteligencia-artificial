556e3c955490b0ff84c1e0e8b1d18bcc673989b9
horbus <renatovargasruiz@hotmail.com>
2019-09-09 21:22:05 -0500

    practica01

---
+++ Steer/Assets/Scripts/Agent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    //Agent 
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+
+    public float maxSpeed = 10;
+    public float maxSteer = 10;
+
+
+    //..
+    Transform target;
+    Transform Area;
+
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+        Area = GameObject.Find("Area").transform;
+
+        float dist = Vector3.Distance(Area.position , transform.position);
+
+        //desired
+        desired = -(target.position - transform.position).normalized * maxSpeed;
+        //steer
+        steer = Vector3.ClampMagnitude(desired - velocity , maxSteer);
+        //velocity
+        velocity += steer * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        if(transform.position.x  <= -12.00 || transform.position.x  >= 12.00 || transform.position.y  <= -5.00 || transform.position.y  >= 5.00 ){
+           Debug.Log("PERDISTE");
+           Destroy(gameObject);
+        }
+
+        //print(Vector3.Distance(Area.position , transform.position));
+
+        if(dist <=1.5 ){
+            Debug.Log("GANASTE");
+            Destroy(gameObject);
+        }
+
+        
+    }
+}
---
+++ Steer/Assets/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{    
+    // float tx = 10;
+    // float ty = 20;
+    // float minX = -5;
+    // float minY = -5;
+    // float width = 10;
+    // float height = 10;
+
+
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.Scale(new Vector3(1,1,0));
+        transform.position = mousePosition;
+
+       // tx += 0.01f;
+       // ty += 0.01f;
+
+       // float x = minX + width * Mathf.PerlinNoise(tx , 0);
+       // float y = minY + height * Mathf.PerlinNoise(0 , ty);
+      //  transform.position = new Vector3(x,y,0);
+       
+    }
+}
