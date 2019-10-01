34acf90a706f19e409b026f397ad556d2370c93b
horbus <renatovargasruiz@hotmail.com>
2019-09-30 21:36:44 -0500

    practica02

---
+++ practica2/Assets/Agent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Agent : SBAgent
+{
+	public Transform target;
+	public Transform Originpoint;
+	
+
+	void Start()
+	{
+		maxSpeed = 5f;
+		maxSteer = 5f;
+		
+	}
+
+	void Update()
+	{		
+		if(Vector3.Distance(Originpoint.position , transform.position) > 2){
+			velocity += SteeringBehaviours.Seek(this, Originpoint, 100);
+			transform.position += velocity* Time.deltaTime;	
+		}
+
+	    velocity += SteeringBehaviours.Seek(this, target, 100);
+		transform.position += velocity* Time.deltaTime;		
+	}
+}
---
+++ practica2/Assets/Agent2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Agent2 : SBAgent
+{
+	public Transform target;
+	public Transform Originpoint;
+	public float areaCubo;
+	
+
+	void Start()
+	{
+		maxSpeed = 5f;
+		maxSteer = 10f;
+		areaCubo = 2f;
+		
+	}
+
+	void Update()
+	{		
+		if(transform.position.x > areaCubo || transform.position.x < -areaCubo || transform.position.y > areaCubo || transform.position.y < -areaCubo){
+			velocity += SteeringBehaviours.Seek(this, Originpoint, 100);
+			transform.position += velocity* Time.deltaTime;	
+		}
+	    velocity += SteeringBehaviours.Seek(this, target, 100);
+		transform.position += velocity* Time.deltaTime;		
+	}
+}
